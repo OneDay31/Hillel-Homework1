5b85309f5987b089c37bd3e620dc3a2e4afb2ff0
roma <roma.leichenko@gmail.com>
2022-10-09 21:48:54 +0300

    Homework1

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import  this
+Zen of Python, это Дзен написанный Тимом Питерсом, 19 принципов от разработчика, которых должен придерживаться каждый, кто выбрал путь програмирования.
+import antigravity
+Этот код открывает в браузере комикс XKCD под названием "Летающий змей" в котором упоминается "Python" этот модуль антигравитации был добавлен в python3 Скипом Монтаро
+import __hello__
+Разработчики скрыли модуль "Hello world" просто импортировав его. повторный импорт не приведет к повторному появлению сообщения. Так же этот код является первой программой для новичков, демонстрируя простоту кода Python
+from __future__ import braces
+В моем понимании данного кода, он пытается импортировать фигурные скобки для будущей функции (кода), в результате получаем ошибку "Not a chance", она заложена самим разработчикам, тем самым дает понять, любителям фигурных скобок в стиле С тут не место.
